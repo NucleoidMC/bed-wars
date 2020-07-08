8dd5077507948b0a69fc29a015d67a7b48aa913d
SuperCoder7979 <k.pranav@gmail.com>
2020-07-08 18:44:36 -0400

    make beds not work in-game

---
+++ src/main/java/net/gegy1000/bedwars/mixin/MixinBedBlock.java
@@ -0,0 +1,31 @@
+package net.gegy1000.bedwars.mixin;
+
+import net.gegy1000.bedwars.game.GameManager;
+import net.gegy1000.bedwars.game.bw.BedWars;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import net.minecraft.block.BedBlock;
+import net.minecraft.block.BlockState;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+@Mixin(BedBlock.class)
+public class MixinBedBlock {
+
+	@Inject(method = "onUse", at = @At("HEAD"))
+	private void injectOnUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit, CallbackInfoReturnable<ActionResult> cir) {
+		if (!world.isClient()) {
+			BedWars game = GameManager.activeFor(BedWars.GAME);
+			if (game != null && game.map.contains(pos)) {
+				cir.setReturnValue(ActionResult.CONSUME);
+			}
+		}
+	}
+}
