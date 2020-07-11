4f794bcda56324d85226f71ce1568777fc58a41e
SuperCoder7979 <k.pranav@gmail.com>
2020-07-11 09:54:58 -0400

    nether portals are no more

---
+++ src/main/java/net/gegy1000/bedwars/mixin/MixinNetherPortalBlock.java
@@ -0,0 +1,23 @@
+package net.gegy1000.bedwars.mixin;
+
+import net.gegy1000.bedwars.game.GameManager;
+import net.gegy1000.bedwars.game.bw.BedWars;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import net.minecraft.block.NetherPortalBlock;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.WorldAccess;
+
+@Mixin(NetherPortalBlock.class)
+public class MixinNetherPortalBlock {
+	@Inject(method = "createPortalAt", at = @At("HEAD"), cancellable = true)
+	private static void handleCreatePortal(WorldAccess world, BlockPos pos, CallbackInfoReturnable<Boolean> cir) {
+		BedWars game = GameManager.activeFor(BedWars.TYPE);
+		if (game != null && game.map.contains(pos)) {
+			cir.setReturnValue(false);
+		}
+	}
+}
