3f4b30a523e9cbbe13aeb12e6ae4929f96541aea
SuperCoder7979 <k.pranav@gmail.com>
2020-12-21 19:17:43 -0500

    nerfed item spawn rates

--- src/main/java/xyz/nucleoid/bedwars/game/active/ItemGeneratorPool.java
+++ src/main/java/xyz/nucleoid/bedwars/game/active/ItemGeneratorPool.java
@@ -8,32 +8,31 @@ import java.util.Random;
 
 public final class ItemGeneratorPool {
     public static final ItemGeneratorPool TEAM_LVL_1 = new ItemGeneratorPool()
-            .add(new ItemStack(Items.IRON_INGOT, 1), 10)
-            .add(new ItemStack(Items.GOLD_INGOT, 1), 3)
-            .spawnInterval(30);
+            .add(new ItemStack(Items.IRON_INGOT, 1), 12)
+            .add(new ItemStack(Items.GOLD_INGOT, 1), 2)
+            .spawnInterval(36);
 
     public static final ItemGeneratorPool TEAM_LVL_2 = new ItemGeneratorPool()
             .add(new ItemStack(Items.IRON_INGOT, 1), 10)
+            .add(new ItemStack(Items.IRON_INGOT, 2), 4)
             .add(new ItemStack(Items.GOLD_INGOT, 1), 3)
-            .add(new ItemStack(Items.IRON_INGOT, 2), 8)
-            .add(new ItemStack(Items.GOLD_INGOT, 2), 2)
-            .spawnInterval(26);
+            .spawnInterval(30);
 
     public static final ItemGeneratorPool TEAM_LVL_3 = new ItemGeneratorPool()
-            .add(new ItemStack(Items.IRON_INGOT, 1), 10)
-            .add(new ItemStack(Items.GOLD_INGOT, 1), 3)
-            .add(new ItemStack(Items.IRON_INGOT, 2), 8)
-            .add(new ItemStack(Items.GOLD_INGOT, 2), 2)
+            .add(new ItemStack(Items.IRON_INGOT, 1), 36)
+            .add(new ItemStack(Items.IRON_INGOT, 2), 18)
+            .add(new ItemStack(Items.GOLD_INGOT, 1), 9)
+            .add(new ItemStack(Items.GOLD_INGOT, 2), 3)
             .add(new ItemStack(Items.EMERALD, 1), 1)
-            .spawnInterval(22);
+            .spawnInterval(26);
 
     public static final ItemGeneratorPool DIAMOND = new ItemGeneratorPool()
             .add(new ItemStack(Items.DIAMOND, 1), 1)
-            .spawnInterval(20 * 40);
+            .spawnInterval(20 * 45);
 
     public static final ItemGeneratorPool EMERALD = new ItemGeneratorPool()
             .add(new ItemStack(Items.EMERALD, 1), 1)
-            .spawnInterval(20 * 80);
+            .spawnInterval(20 * 90);
 
     private final WeightedList<ItemStack> pool = new WeightedList<>();
     private long spawnInterval = 10;
