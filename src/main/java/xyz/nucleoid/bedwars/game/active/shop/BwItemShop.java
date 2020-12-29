1cc0ff3d3c85804e43ec4d126d9ece8152407c20
yitzy299 <hugocarter@hotmail.co.uk>
2020-12-29 17:26:29 +0000

    Update BwItemShop.java
    Remove redundant code

--- src/main/java/xyz/nucleoid/bedwars/game/active/shop/BwItemShop.java
+++ src/main/java/xyz/nucleoid/bedwars/game/active/shop/BwItemShop.java
@@ -272,26 +272,5 @@ public final class BwItemShop {
             }
         });
     }
-    //TODO add to the plasmid colored blocks library
-    public static Block glassPane(DyeColor color) {
-        switch (color) {
-            case ORANGE: return Blocks.ORANGE_STAINED_GLASS;
-            case MAGENTA: return Blocks.MAGENTA_STAINED_GLASS;
-            case LIGHT_BLUE: return Blocks.LIGHT_BLUE_STAINED_GLASS;
-            case YELLOW: return Blocks.YELLOW_STAINED_GLASS;
-            case LIME: return Blocks.LIME_STAINED_GLASS;
-            case PINK: return Blocks.PINK_STAINED_GLASS;
-            case GRAY: return Blocks.GRAY_STAINED_GLASS;
-            case LIGHT_GRAY: return Blocks.LIGHT_GRAY_STAINED_GLASS;
-            case CYAN: return Blocks.CYAN_STAINED_GLASS;
-            case PURPLE: return Blocks.PURPLE_STAINED_GLASS;
-            case BLUE: return Blocks.BLUE_STAINED_GLASS;
-            case BROWN: return Blocks.BROWN_STAINED_GLASS;
-            case GREEN: return Blocks.GREEN_STAINED_GLASS;
-            case RED: return Blocks.RED_STAINED_GLASS;
-            case BLACK: return Blocks.BLACK_STAINED_GLASS;
-            case WHITE: return Blocks.WHITE_STAINED_GLASS;
-            default: return Blocks.GLASS;
-        }
-    }
+
 }
