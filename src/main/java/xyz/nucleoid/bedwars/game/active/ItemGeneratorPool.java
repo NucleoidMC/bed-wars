56c59f46a83906a3e59b41f324fbee1b4a88711c
Siuol <43890828+Siuolthepic@users.noreply.github.com>
2021-05-08 14:30:19 -0500

    Iron go zoomer now
    In this commit I did the following
    
    Revert previous changes.
    Replaced them with a general speed increase to generation.
    
    Haha Iron go zoooooooooom

--- src/main/java/xyz/nucleoid/bedwars/game/active/ItemGeneratorPool.java
+++ src/main/java/xyz/nucleoid/bedwars/game/active/ItemGeneratorPool.java
@@ -8,15 +8,15 @@ import java.util.Random;
 
 public final class ItemGeneratorPool {
     public static final ItemGeneratorPool TEAM_LVL_1 = new ItemGeneratorPool()
-            .add(new ItemStack(Items.IRON_INGOT, 2), 10)
+            .add(new ItemStack(Items.IRON_INGOT, 1), 10)
             .add(new ItemStack(Items.GOLD_INGOT, 1), 2)
-            .spawnInterval(34);
+            .spawnInterval(26);
 
     public static final ItemGeneratorPool TEAM_LVL_2 = new ItemGeneratorPool()
-            .add(new ItemStack(Items.IRON_INGOT, 2), 10)
-            .add(new ItemStack(Items.IRON_INGOT, 4), 3)
+            .add(new ItemStack(Items.IRON_INGOT, 1), 10)
+            .add(new ItemStack(Items.IRON_INGOT, 2), 4)
             .add(new ItemStack(Items.GOLD_INGOT, 1), 3)
-            .spawnInterval(30);
+            .spawnInterval(24);
 
     public static final ItemGeneratorPool TEAM_LVL_3 = new ItemGeneratorPool()
             .add(new ItemStack(Items.IRON_INGOT, 2), 36)
@@ -24,7 +24,7 @@ public final class ItemGeneratorPool {
             .add(new ItemStack(Items.GOLD_INGOT, 1), 9)
             .add(new ItemStack(Items.GOLD_INGOT, 2), 3)
             .add(new ItemStack(Items.EMERALD, 1), 1)
-            .spawnInterval(26);
+            .spawnInterval(20);
 
     public static final ItemGeneratorPool DIAMOND = new ItemGeneratorPool()
             .add(new ItemStack(Items.DIAMOND, 1), 1)
